967f47c0ea7fa8d869dbbc328ea00dfba74f0785
Aakash K H <157706554+etherealkh07@users.noreply.github.com>
2024-12-20 11:29:35 +0530

    Update Driver.java

--- Driver/Driver.java
+++ Driver/Driver.java
@@ -7,12 +7,17 @@ import com.EntityPackage.Services.Service;
 public class Driver {
 
     public static void main(String[] args) {
+    	boolean hi = true;
+    	while(hi == true )
+    	{
         Scanner scn = new Scanner(System.in);
         System.out.println("Select the option:");
         System.out.println("Press 1 to Save");
         System.out.println("Press 2 to Update");
         System.out.println("Press 3 to Fetch");
         System.out.println("Press 4 to Delete");
+        System.out.println("Press 5 to Exit");
+        
 
         System.out.print("Enter the number: ");
         int num = scn.nextInt();
@@ -30,8 +35,14 @@ public class Driver {
             case 4:
                  Service.delete();
                 break;
+            case 5:
+            	hi=false;
+            	System.out.println("exited");
+            	break;
             default:
                 System.out.println("Invalid Number");
         }
+        
+    	}
     }
 }
